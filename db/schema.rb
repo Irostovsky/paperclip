b62e9ccca438aa47cffb2922ce12eed97acce583
Ivan Rostovsky <ir@anahoret.com>
2010-01-18 07:20:58 +0200

    Add photo

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100118051016) do
+ActiveRecord::Schema.define(:version => 20100118051633) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -17,6 +17,10 @@ ActiveRecord::Schema.define(:version => 20100118051016) do
     t.boolean  "sale"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
 end
